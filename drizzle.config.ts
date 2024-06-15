9487b87a1af8e1cc108f528de2543f2c649f5955
vineboneto <vineboneto@gmail.com>
2024-06-15 04:04:01 -0300

    feat: removido env desnecessario

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,5 +1,6 @@
-import "./lib/drizzle/env";
+import dotenv from "dotenv";
 import { defineConfig } from "drizzle-kit";
+dotenv.config({ path: ".env.local" });
 
 export default defineConfig({
   schema: "./lib/drizzle/schema.ts",
